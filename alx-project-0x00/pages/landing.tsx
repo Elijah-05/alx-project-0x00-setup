b3a4d9b86704dca87698d362737d39ae16e17267
Elijah Abebe <ellaab804@gmail.com>
2025-09-17 23:31:55 +0300

    feat: custom button with props

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,26 @@
-import Card from "@/components/Card"
+import Button from "@/components/Button";
+import Card from "@/components/Card";
 
-const Landing: React.FC =  () => {
+const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+
+      {/* === Size Variants === */}
+      <div className="space-x-4">
+        <Button title="Small" size="small" />
+        <Button title="Medium" size="medium" />
+        <Button title="Large" size="large" />
+      </div>
+
+      {/* === Rounded Variants === */}
+      <div className="space-x-4 mt-6">
+        <Button title="Rounded SM" rounded="sm" />
+        <Button title="Rounded MD" rounded="md" />
+        <Button title="Rounded Full" rounded="full" />
+      </div>
     </div>
-  )
-}
-export default Landing
+  );
+};
+export default Landing;
